4b96dbb3899f93b9483b5a90e774d49d02d69d46
takahiro0713 <literary.and.military.arts0713@gmail.com>
2022-09-15 22:36:51 +0900

    クラスの追加

---
+++ src/CountructionFee.java
@@ -0,0 +1,13 @@
+
+public class CountructionFee extends Material{
+	private int timeZone;
+
+	public int getTimeZone() {
+		return timeZone;
+	}
+
+	public void setTimeZone(int timeZone) {
+		this.timeZone = timeZone;
+	}
+	
+}
--- src/DesignSystem.java
+++ src/DesignSystem.java
@@ -2,8 +2,7 @@
 public class DesignSystem {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		
+	
 	}
 
 }
