6ad666581917358fa33a3a8c431eadebffbf8e22
takahiro0713 <literary.and.military.arts0713@gmail.com>
2022-09-18 02:22:29 +0900

    実装１回目

--- src/DesinDocument.java
+++ src/DesinDocument.java
@@ -1,4 +1,25 @@
 
 public class DesinDocument {
-
+	
+	private UtilityPole poleInfo;
+	
+	public DesinDocument() {
+		poleInfo = new UtilityPole();
+		
+		poleInfo.setPoleNo(0);
+		poleInfo.setZahyoX(0);
+		poleInfo.setZahyoY(0);
+		
+	}
+	
+	public void makePole(int zahyoX,int zahyoY,int poleNo) {
+		poleInfo = new UtilityPole();
+		
+		poleInfo.setPoleNo(zahyoX);
+		poleInfo.setZahyoX(zahyoY);
+		poleInfo.setZahyoY(poleNo);
+		
+		//電柱画面表示
+	}
+	
 }
