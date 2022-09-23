7e6fdd091d116e8d4c8d9dd88cda436e17e84325
takahiro0713 <literary.and.military.arts0713@gmail.com>
2022-09-23 11:25:51 +0900

    ２回目の実装

---
+++ Design System/src/SuppliersMaterials.java
@@ -0,0 +1,13 @@
+
+public class SuppliersMaterials extends Material {
+	private int commision;
+
+	public int getCommision() {
+		return commision;
+	}
+
+	public void setCommision(int commision) {
+		this.commision = commision;
+	}
+	
+}
